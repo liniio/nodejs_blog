5e5bfc2ae99530a9a5970d176f42273a1cb5720c
Ngoc Linh <ngoclinh.workcontact@gmail.com>
2024-07-30 00:59:28 +0700

    Create routes, controller folders

---
+++ src/app/controllers/NewController.js
@@ -0,0 +1,14 @@
+class NewControllers {
+
+    // [GET] /new
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [GET] /new/:slug
+    show(req, res) {
+        res.send('NEW DETAIL!!!');
+    }
+}
+
+module.exports = new NewControllers();
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
@@ -15,13 +17,8 @@ app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
